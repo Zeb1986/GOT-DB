b09204071661a10d9f428dced4e4d80040a4f9a3
Viaches <viache86@gmail.com>
2021-03-26 12:21:05 +0200

    Initial commit

---
+++ src/components/itemList/itemList.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import './itemList.css';
+import Spinner from '../spinner';
+
+function ItemList({getData, onItemSelected, renderItem}) {
+
+    const [ItemList, updateList] = useState([]);
+
+    useEffect(() => {
+        getData()
+        .then((data) => {
+            updateList(data)
+            })
+        }, [])
+
+    function renderItems(arr) {
+        return arr.map((item) => {
+            const {id} = item
+            const label = renderItem(item);
+            return (
+                <li 
+                key={id}
+                className="list-group-item"
+                onClick={() => onItemSelected(id)}
+                >
+                    {label}
+                </li>
+            );
+        })
+    }
+        
+
+        if (!ItemList) {
+            return <Spinner/>
+        }
+
+        const items = renderItems(ItemList)
+
+        return (
+            <ul className="item-list list-group">
+               {items}
+            </ul>
+        );
+    
+}
+
+export default ItemList;
+
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spinner-uhbipyb33"><div className="ldio-z72kedl7grn">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
